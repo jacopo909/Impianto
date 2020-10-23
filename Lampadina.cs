5696c7007c2ddc6b4f19113d38aa90ea9d18d55b
jacopo909 <60032905+jacopo909@users.noreply.github.com>
2020-10-23 20:41:40 +0200

    Add files via upload

---
+++ Lampadina.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Impianto
+{
+    class Lampadina
+    {
+        private static int accesa = 1;
+        private static int spenta = 0;
+        private const int rotta = 2;
+        private static int rottura = 50;
+        private int click = 0;
+        private int stato = spenta;
+
+
+        public int NumeroClick { get; set; }
+
+
+        public Lampadina() { }
+
+        public int Click()
+        {
+
+            click = click + 1;
+            if (click >= rottura)
+            {
+                stato = rotta;
+            }
+            else
+            {
+                if (stato == spenta)
+                {
+                    stato = accesa;
+                }
+                else
+                {
+                    stato = spenta;
+                }
+            }
+
+            return stato;
+        }
+        public string Visualizza()
+        {
+            if (stato == rotta)
+            {
+                return "La lampadina è rotta";
+            }
+            if (stato == accesa)
+            {
+                return "La lampadina è accesa";
+            }
+            else
+            {
+                return "La lampadina è spenta";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Impianto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lampadina l = new Lampadina();
+            Console.WriteLine("Quanti Click vuoi effettuare?");
+            l.NumeroClick = int.Parse(Console.ReadLine());
+            for (int i = 0; i < l.NumeroClick; i++)
+            {
+                l.Click();
+                Console.WriteLine(l.Visualizza());
+               
+            }
+        }
+    }
+}
